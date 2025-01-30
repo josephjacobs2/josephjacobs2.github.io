4cfdc9e03af61048219d837daade069065d9076b
josephjacobs2 <josephjacobs2@mail.weber.edu>
2025-01-29 21:06:56 -0700

    Shape Area and Volume Calculator

--- scripts.js
+++ scripts.js
@@ -10,3 +10,18 @@ function setDefaultTheme() {
     document.getElementById('ButtonsStatus').innerHTML = "Theme Buttons - Currently Default";
     document.getElementById('body').style = "background-color: lightblue";
 }
+function calculatePerimeter() {
+    let radius = Number(document.getElementById("RadiusInput").value);
+    let perimeter = radius * 2 * Math.PI;
+    document.getElementById("PerimeterTitle").innerHTML = "Perimeter = " + perimeter;
+}
+function calculateArea() {
+    let radius = Number(document.getElementById("RadiusInput").value);
+    let area = radius ** 2 * Math.PI;
+    document.getElementById("AreaTitle").innerHTML = "Area = " + area;
+}
+function calculateVolume() {
+    let radius = Number(document.getElementById("RadiusInput").value);
+    let volume = radius ** 3 * Math.PI * (4.0 / 3.0);
+    document.getElementById("VolumeTitle").innerHTML = "Volume = " + volume;
+}
